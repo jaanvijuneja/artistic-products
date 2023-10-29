267fef0aa0bb152081f6b29008002daa8345aed2
Jaanvi Juneja <junejajaanvi@gmail.com>
2023-10-29 10:51:25 -0400

    pipe JSON outputs through jq

--- tests/bad_params.sh
+++ tests/bad_params.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 curl -v -d @'bad_params.json' -H 'Content-Type: application/json' \
-     'http://127.0.0.1:8000'
-echo
+     'http://127.0.0.1:8000' | jq .
--- tests/missing_item.sh
+++ tests/missing_item.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 curl -v -d @'missing_item.json' -H 'Content-Type: application/json' \
-     'http://127.0.0.1:8000'
-echo
+     'http://127.0.0.1:8000' | jq .
